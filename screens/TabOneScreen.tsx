e5b832c9a618a5dc3f98d633cd9257492a319652
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2020-07-11 17:10:39 +0900

    record event

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button, Alert } from 'react-native';
+import * as Analytics from 'expo-firebase-analytics';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
+  const recordOnPressLog = () => {
+    Analytics.logEvent('onPress');
+    Alert.alert('OnPress');
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <Button onPress={recordOnPressLog} title="Record Log Event" />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
